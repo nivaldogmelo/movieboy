db0795723406181da5c76321e8cffc6a57c3f6bf
Nivaldo Melo <nivaldogmelo@gmail.com>
2019-12-26 21:36:51 -0300

    Add initial main file, along with go mod settings

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/nivaldogmelo/abed
+
+go 1.13
+
+require gopkg.in/tucnak/telebot.v2 v2.0.0-20191225234705-baa616bc00d5 // indirect
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+        "os"
+        tb "gopkg.in/tucnak/telebot.v2"
+        "log"
+)
+
+func main() {
+        var (
+                port      = os.Getenv("PORT")
+                publicURL = os.Getenv("PUBLIC_URL")
+                token     = os.Getenv("TOKEN")
+        )
+
+        webhook := &tb.Webhook{
+                Listen: ":" + port,
+                Endpoint: &tb.WebhookEnpoint{PublicURL: publicURL},
+        }
+
+        pref := tb.Settings{
+                Token: token,
+                Poller: webhook,
+        }
+
+        b, err := tb.NewBot(pref)
+        if err != nil {
+                log.Fatal(err)
+        }
+
+        b.Handle("/hello". func(m *tb.Message) {
+                b.Send(m.Sender, "Hi!")
+        })
+}
+
